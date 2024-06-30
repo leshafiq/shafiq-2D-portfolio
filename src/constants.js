96911d261227ec701542bf6662cd129490124141
leshafiq <leshafiq96@gmail.com>
2024-06-30 20:48:57 +0800

    first commit

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I am currently using Proxmox as my Homelab running multiple docker containers for my needs, one of which is this 2D portfolio which is running in one of my virtual machines (container).
+    You can visit my github for my full projects where I will explain how to setup a simple homelab for a beginner like me.
+    Here is my <a href="https://github.com/leshafiq/prox-homelab" target="_blank">Github</a>!`,
+  "cs-degree": `This is my Computer Science degree and Electronic Engineering Diploma. Even though I am not working as a software engineer right now but it gave me a good foundation for what I am working now.`,
+  "sofa-table": `That's my sofa. I spent a lot of my time here watching movies and anime on the weekend.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@LinusTechTips" target="_blank">Linus Tech Tips</a>, <a href="https://www.youtube.com/@NetworkChuck" target="_blank">Network Chuck</a>,
+  <a href="https://www.youtube.com/@TechnoTim" target="_blank">Techno Tim</a> and <a href="https://www.youtube.com/@christianlempa" target="_blank">Christian Lempa</a> (sometimes) !`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my linkedin resume. <a href="https://www.linkedin.com/in/shafiqyusof96/" target="_blank">Check it out?</a>
+  Contact me at leshafiq96@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching some of JSLegendDev tutorials (the creator of this app) <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `I don't have a lot of books hehe`,
+  exit: `If you want to exit Shafiq's portfolio, just close the tab.`,
+};
